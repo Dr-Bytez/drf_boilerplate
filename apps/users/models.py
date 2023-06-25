c4a54d1e5efa21a0219b518a0c33010774a5e810
new_user <bear.lvvb@mail.ru>
2023-06-25 16:43:22 +0500

    added

---
+++ apps/users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_tokens(self):
+        refresh = RefreshToken.for_user(self)
+
+        tokens = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+        return tokens
